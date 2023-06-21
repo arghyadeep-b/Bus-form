c8adef933f290277f00e737bcaca13e7ee1866f5
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-06-21 16:34:26 +0530

    Bus form ready

--- src/components/BusForm.jsx
+++ src/components/BusForm.jsx
@@ -45,38 +45,58 @@ function BusForm() {
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formData.busNo === "" || formData.route == "" || formData.driver === "")
+      return;
     //? Adding the form data to the list of data
-    setList([...list, formData]);
+    const uniqueList = list.filter((item) => {
+      // Checking if the the bus no or the driver is already in the list
+      return item.busNo != formData.busNo && item.driver != formData.driver;
+    });
+    setList([...uniqueList, formData]);
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.busNo !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.busNo === id);
+    removeItem(id);
+    setFormData(specificItem);
   };
   return (
-    <>
+    <div className="container">
+      <h1 className="bus-form-title">Bus Form</h1>
       <form className="bus-form-container" onSubmit={handleSubmit}>
-        <legend>Bus Form</legend>
-        <label htmlFor="bus-no">
-          Bus number :
+        <div className="bus-no-container input-containers">
+          <label htmlFor="bus-no">Bus number :</label>
           <input
             type="number"
             id="bus-no"
-            placeholder="Bus no"
+            placeholder="Enter Bus no"
             name="busNo"
             value={formData.busNo}
             min="1"
             max="12"
+            className="bus-form-inputs bus-no"
             onChange={handleInputChange}
+            required
           />
-        </label>
+        </div>
 
-        <label htmlFor="route">
-          Route :
+        <div className="route-container input-containers">
+          <label htmlFor="route">Route :</label>
           <select
             name="route"
             id="route"
+            className="bus-form-inputs route"
             onChange={handleInputChange}
             defaultValue="default"
+            required
           >
             <option value="default" disabled>
               --Select Route--
@@ -102,24 +122,28 @@ function BusForm() {
               <option value="extra-4">Extra Place S - Place T</option>
             </select>
           )}
-        </label>
+        </div>
 
-        <label htmlFor="driver-name">
-          Driver's name :
+        <div className="driver-container input-containers">
+          <label htmlFor="driver-name">Driver's name :</label>
           <input
             type="text"
             name="driver"
             id="driver-name"
+            placeholder="Enter a name"
+            className="bus-form-inputs driver-name"
             onChange={handleInputChange}
+            required
           />
-        </label>
-
-        <button type="submit">Submit</button>
-        {console.table("List", list)}
+        </div>
       </form>
+      <button type="submit" className="btn-submit" onClick={handleSubmit}>
+        Add Bus
+      </button>
+      {console.table("List", list)}
 
-      <BusList list={list} />
-    </>
+      <BusList list={list} removeItem={removeItem} editItem={editItem} />
+    </div>
   );
 }
 
--- src/components/BusList.jsx
+++ src/components/BusList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import "./BusList.css";
 
-function BusList({ list }) {
+function BusList({ list, removeItem, editItem }) {
   return (
     <div className="bus-list">
       <h2 className="bus-list-title">Bus List</h2>
@@ -8,12 +10,30 @@ function BusList({ list }) {
       {list.map((item, index) => {
         return (
           <>
-            <h4>Item {index + 1}</h4>
-            <ul key={index}>
-              <li>Bus number: {item.busNo}</li>
-              <li>Route: {item.route}</li>
-              <li>Driver: {item.driver}</li>
-            </ul>
+            <div key={index} className="bus-list-item ticket">
+              <div className="ticket-bus-no">
+                <p>Bus No. {item.busNo}</p>
+                <div className="style-zigzag"></div>
+              </div>
+              <div>
+                <li>Route: {item.route}</li>
+                <li>Driver: {item.driver}</li>
+              </div>
+              <button
+                type="button"
+                className="btn-edit-ticket"
+                onClick={() => editItem(item.busNo)}
+              >
+                <FaEdit />
+              </button>
+              <button
+                type="button"
+                className="btn-delete-ticket"
+                onClick={() => removeItem(item.busNo)}
+              >
+                <FaTrash />
+              </button>
+            </div>
           </>
         );
       })}
